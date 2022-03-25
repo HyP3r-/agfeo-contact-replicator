98e5ed02da0de764d1d2f085ee3607eaf36be7d6
Andreas Fendt <mail@andreas-fendt.de>
2022-03-25 23:03:52 +0100

    agefo-contact.py: rewrote to a class based system... maybe in future we can run this as service or so config.toml: created external config for easier maintainance requirements.txt: added toml .gitignore: created

--- agefo-contact.py
+++ agefo-contact.py
@@ -3,7 +3,9 @@ Creates from different sources a VCF-File for Import
 """
 import inspect
 import os
+from typing import List
 
+import toml as toml
 from exchangelib import Credentials, Account
 from sqlalchemy import create_engine, Table, MetaData, select, and_, or_
 import vobject.vcard
@@ -18,69 +20,113 @@ __status__ = "Production"
 
 current_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-# get exchange contacts
-exchange_credentials = Credentials('info@elektro-fendt.de', 'CyTfBKPmDZ6yJ1JpzKYY')
-exchange_account = Account('info@elektro-fendt.de', credentials=exchange_credentials, autodiscover=True)
-exchange_contacts = [contact for contact in exchange_account.contacts.all()]
-exchange_contacts_str = "".join([bytes(exchange_contact.mime_content).decode()
-                                 for exchange_contact in exchange_contacts])
 
-# get sage contacts
-engine = create_engine("mssql+pyodbc://sa:Fezie1958@EF-PC01\\SAGEHW/Handwerk?driver=ODBC+Driver+13+for+SQL+Server",
-                       echo=True)
-conn = engine.connect()
+class AgfeoContact:
 
-metadata_obj = MetaData()
-customers = Table("T_KND", metadata_obj, autoload_with=engine)
+    def __init__(self):
+        config = os.path.join(current_path, "config.toml")
+        self.config = toml.load(config)
 
-# noinspection PyPropertyAccess
-stmt = select(
-    (customers.c.Name).label("name"),
-    (customers.c.Telefon1).label("phone_1"),
-    (customers.c.Telefon2).label("phone_2")
-).where(
-    and_(customers.c.Name != None,
-         or_(customers.c.Telefon1 != None, customers.c.Telefon1 != None))
-).order_by(customers.c.Name)
+    def run(self):
+        """
+        Run targets and write file
+        """
 
-sage_contacts = []
+        contacts = []
+        for run_target in [self.run_sage, self.run_exchange]:
+            contacts = contacts + run_target()
 
-for row in conn.execute(stmt):
-    sage_vcard = vobject.vCard()
+        contacts_str = "".join([contact.serialize() for contact in contacts])
 
-    sage_vcard_object = sage_vcard.add("n")
-    sage_vcard_object.value = vobject.vcard.Name(family=row.name)
+        # write result to file
+        with open(os.path.join(current_path, "result", "result.vcf"), "w", encoding="utf8", newline="") as f:
+            f.write(contacts_str)
 
-    sage_vcard_object = sage_vcard.add("fn")
-    sage_vcard_object.value = row.name
+    def run_exchange(self) -> List:
+        """
+        Get all Exchange contacts
+        """
 
-    found = False
-    for number in [row.phone_1, row.phone_2]:
-        if number is None or number == "":
-            continue
+        config_exchange = self.config["exchange"]
 
-        try:
-            phonenumber = phonenumbers.parse(number, "DE")
-        except:
-            continue
+        # get exchange contacts
+        exchange_credentials = Credentials(config_exchange["username"], config_exchange["password"])
+        exchange_account = Account(config_exchange["username"], credentials=exchange_credentials, autodiscover=True)
+        exchange_contacts = []
+        for exchange_contact in exchange_account.contacts.all():
+            try:
+                v = vobject.readOne(bytes(exchange_contact.mime_content).decode())
+                exchange_contacts.append(v)
+            except:
+                pass
+        return exchange_contacts
 
-        if not phonenumbers.is_valid_number(phonenumber) or not phonenumbers.is_possible_number(phonenumber):
-            continue
+    def run_sage(self) -> List:
+        """
+        Process all Sage contacts
+        """
 
-        found = True
-        phonenumber_str = phonenumbers.format_number(phonenumber, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
-        sage_vcard_object = sage_vcard.add("tel")
+        config_sage = self.config["sage"]
 
-        sage_vcard_object.type_param = "CELL" \
-            if phonenumbers.carrier._is_mobile(phonenumbers.phonenumberutil.number_type(phonenumber)) \
-            else "WORK"
-        sage_vcard_object.value = phonenumber_str
+        # get sage contacts
+        engine = create_engine(
+            f"mssql+pyodbc://{config_sage['username']}:{config_sage['password']}@"
+            f"{config_sage['hostname']}\\{config_sage['instance']}/{config_sage['database']}"
+            f"?driver=ODBC+Driver+13+for+SQL+Server")
+        conn = engine.connect()
 
-    if found:
-        sage_contacts.append(sage_vcard.serialize())
+        metadata_obj = MetaData()
+        customers = Table("T_KND", metadata_obj, autoload_with=engine)
 
-sage_contacts_str = "".join(sage_contacts)
+        # noinspection PyPropertyAccess
+        stmt = select(
+            (customers.c.Name).label("name"),
+            (customers.c.Telefon1).label("phone_1"),
+            (customers.c.Telefon2).label("phone_2")
+        ).where(
+            and_(customers.c.Name != None,
+                 or_(customers.c.Telefon1 != None, customers.c.Telefon1 != None))
+        ).order_by(customers.c.Name)
 
-# write result to file
-with open(os.path.join(current_path, "result", "result.vcf"), "w", encoding="utf8", newline="") as f:
-    f.write("".join([exchange_contacts_str, sage_contacts_str]))
+        sage_contacts = []
+
+        for row in conn.execute(stmt):
+            sage_vcard = vobject.vCard()
+
+            sage_vcard_object = sage_vcard.add("n")
+            sage_vcard_object.value = vobject.vcard.Name(family=row.name)
+
+            sage_vcard_object = sage_vcard.add("fn")
+            sage_vcard_object.value = row.name
+
+            found = False
+            for number in [row.phone_1, row.phone_2]:
+                if number is None or number == "":
+                    continue
+
+                try:
+                    phonenumber = phonenumbers.parse(number, "DE")
+                except:
+                    continue
+
+                if not phonenumbers.is_valid_number(phonenumber) or not phonenumbers.is_possible_number(phonenumber):
+                    continue
+
+                found = True
+                phonenumber_str = phonenumbers.format_number(phonenumber, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+                sage_vcard_object = sage_vcard.add("tel")
+
+                sage_vcard_object.type_param = "CELL" \
+                    if phonenumbers.carrier._is_mobile(phonenumbers.phonenumberutil.number_type(phonenumber)) \
+                    else "WORK"
+                sage_vcard_object.value = phonenumber_str
+
+            if found:
+                sage_contacts.append(sage_vcard)
+
+        return sage_contacts
+
+
+if __name__ == "__main__":
+    agfeo_contact = AgfeoContact()
+    agfeo_contact.run()
